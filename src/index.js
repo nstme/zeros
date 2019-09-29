4147f45e2cd4c672adb76d1a7e07df733f9df952
Anastasiia Sierbaieva <51336515+nstme@users.noreply.github.com>
2019-09-28 22:18:37 -0700

    add: complete zero task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function zeros(expression) {
-  // your solution
+  
+  function factorial(n) {
+    return (BigInt(n) != 1n) ? BigInt(n) * factorial(BigInt(n) - 1n) : 1n;
+  }
+
+  function doubleFactorial(n) {
+    return (BigInt(n) >= 1n) ? BigInt(n) * doubleFactorial(BigInt(n) - 2n) : 1n;
+  }
+
+  let array = expression.split('*');
+
+  let factArray = array.map(item => {
+    if (item.slice(-2) === '!!') {
+      return BigInt(doubleFactorial(parseInt(item)));
+    }
+    return BigInt(factorial(parseInt(item)));
+  });
+
+  mult = BigInt(factArray.reduce((mult, current) => mult * current, 1n));
+
+ 
+  function countZeros(number) {
+    let str = number.toString();
+    let counter = 0;
+
+    for (let i = str.length; i > 1; i--) {
+
+      if (str.charAt(i) == false) {
+        counter += 1;
+      } else {
+        return counter - 1;
+      }
+    }
+    return counter - 1;
+  }
+
+  return countZeros(mult);
 }
